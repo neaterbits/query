4665049b6b015fb34631ed9a4700a685ad1e8a8d
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-02-03 13:35:25 +0100

    having clause only after group by

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedProcessResult_All_Named.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedProcessResult_All_Named.java
@@ -2,7 +2,6 @@ package com.neaterbits.query.sql.dsl.api;
 
 public interface ISharedProcessResult_All_Named<MODEL, RESULT> 
 		extends ISharedProcessResult_GroupBy_Named<MODEL, RESULT>,
-				ISharedProcessResult_Having_Named<MODEL, RESULT>,
 				ISharedProcessResult_OrderBy_Named<MODEL, RESULT> {
 
 }
