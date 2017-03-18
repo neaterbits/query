b4f7edd431d28b72c4f3d9fef212b89e8e9d3516
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-18 14:05:24 +0100

    Added generated tests

---
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereOrGroupByHavingOrderByTest.java
@@ -0,0 +1,21 @@
+package com.neaterbits.query.dsl.gen_test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+
+
+class MappedWhereOrGroupByHavingOrderByTest extends GEN_BaseTestCase {
+
+
+    @Test
+    void testMappedMultiNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testMappedMultiAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+}
---
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereTest.java
@@ -0,0 +1,81 @@
+package com.neaterbits.query.dsl.gen_test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+
+
+class MappedWhereTest extends GEN_BaseTestCase {
+
+
+    @Test
+    void testEntitySingleNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testEntitySingleAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testEntityMultiNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testEntityMultiAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testMappedSingleNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testMappedSingleAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testMappedMultiNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testMappedMultiAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testAggregateSingleNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testAggregateSingleAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testAggregateMultiNamed() {
+        assertThat(true).isEqualTo(false);
+    }
+
+
+    @Test
+    void testAggregateMultiAlias() {
+        assertThat(true).isEqualTo(false);
+    }
+}
