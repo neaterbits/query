0919a2d2486eeaa404a4fe7968dddc8c61f9ce01
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-19 15:27:46 +0100

    More tests

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereTest.java
@@ -3,6 +3,10 @@ package com.neaterbits.query.dsl.gen_test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.MultiBuilt;
+import com.neaterbits.query.sql.dsl.api.SingleBuilt;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.mapped.FarmInfo;
 
 
 public class EntityWhereTest extends GEN_BaseTestCase {
@@ -10,24 +14,80 @@ public class EntityWhereTest extends GEN_BaseTestCase {
 
     @Test
     public void testEntitySingleNamed() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Hill Valley");
+    	final Farm farm2 = new Farm("Table Mountain");
+    	final Farm farm3 = new Farm("Snowy Hills");
+    	
+    	final SingleBuilt<Farm> query = select.one(Farm.class)
+    			
+    			.where(Farm::getName).startsWith("Table")
+    			.build(); 
+
+    	store(farm1, farm2, farm3)
+    	.checkOne(query, new Farm(farm2.getId(), "Table Mountain"));
     }
 
 
     @Test
     public void testEntitySingleAlias() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Hill Valley");
+    	final Farm farm2 = new Farm("Table Mountain");
+    	final Farm farm3 = new Farm("Snowy Hills");
+
+    	final Farm f = select.alias(Farm.class);
+    	
+    	final SingleBuilt<Farm> query = select.one(Farm.class)
+    			
+    			.where(f::getName).startsWith("Table")
+    			.build(); 
+
+    	store(farm1, farm2, farm3)
+    	.checkOne(query, new Farm(farm2.getId(), "Table Mountain"));
     }
 
 
     @Test
     public void testEntityMultiNamed() {
-        assertThat(true).isEqualTo(false);
+    	
+    	assertThat(true).isEqualTo(false);
+    	/*
+    	final Farm farm1 = new Farm("Hill Valley");
+    	final Farm farm2 = new Farm("Table Mountain");
+    	final Farm farm3 = new Farm("Snowy Hills");
+    	
+    	final MultiBuilt<Farm> query = select.list(Farm.class)
+    			.where(Farm::getName).contains("Hill")
+    			.build(); 
+
+    	store(farm1, farm2, farm3)
+    	.checkListUnordered(
+    			query,
+    			new FarmInfo("Hill Valley"),
+    			new FarmInfo("Snowy Hills"));
+    			*/
     }
 
 
     @Test
     public void testEntityMultiAlias() {
-        assertThat(true).isEqualTo(false);
+    	assertThat(true).isEqualTo(false);
+    	
+    	/*
+    	final Farm farm1 = new Farm("Hill Valley");
+    	final Farm farm2 = new Farm("Table Mountain");
+    	final Farm farm3 = new Farm("Snowy Hills");
+    	
+    	final Farm f = select.alias(Farm.class);
+    	
+    	final MultiBuilt<Farm> query = select.list(Farm.class)
+    			.where(f::getName).contains("Hill")
+    			.build(); 
+
+    	store(farm1, farm2, farm3)
+    	.checkListUnordered(
+    			query,
+    			new FarmInfo("Hill Valley"),
+    			new FarmInfo("Snowy Hills"));
+			*/
     }
 }
