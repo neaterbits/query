62f541ddb7a970b805cf968cbf0c072dc36e5ac1
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-19 08:14:40 +0100

    Test model for mapped query and check single named

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereTest.java
@@ -3,6 +3,9 @@ package com.neaterbits.query.dsl.gen_test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.SingleBuilt;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.mapped.FarmInfo;
 
 
 public class MappedWhereTest extends GEN_BaseTestCase {
@@ -10,7 +13,26 @@ public class MappedWhereTest extends GEN_BaseTestCase {
 
     @Test
     public void testMappedSingleNamed() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Hill Valley");
+    	final Farm farm2 = new Farm("Table Mountain");
+    	final Farm farm3 = new Farm("Snowy Hills");
+    	
+    	final SingleBuilt<FarmInfo> query = select.one(FarmInfo.class)
+    			.map(Farm::getName).to(FarmInfo::setName)
+    			.where(Farm::getName).startsWith("Table")
+    			.build(); 
+
+    	store(s -> s
+				.add(farm1)
+				.add(farm2)
+				.add(farm3))
+    	.check(ds ->
+    		checkSelectOneOrNull(
+    				ds,
+    				new FarmInfo("Table Mountain"),
+    				query, 
+    				q -> q.execute())
+		);
     }
 
 
---
+++ test-model/src/main/java/com/neaterbits/query/test/model/mapped/FarmInfo.java
@@ -0,0 +1,52 @@
+package com.neaterbits.query.test.model.mapped;
+
+public class FarmInfo {
+
+	private String name;
+
+	public FarmInfo() {
+		
+	}
+
+	public FarmInfo(String name) {
+		
+		if (name == null) {
+			throw new IllegalArgumentException("name == null");
+		}
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FarmInfo other = (FarmInfo) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
