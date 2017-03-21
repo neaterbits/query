e3acfdc97776bee7a3eda2c507c6547dcf39dd29
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-21 05:54:59 +0100

    Aliased entity default order by

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereOrderByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereOrderByTest.java
@@ -3,6 +3,7 @@ package com.neaterbits.query.dsl.gen_test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.IAlias;
 import com.neaterbits.query.sql.dsl.api.MultiBuilt;
 import com.neaterbits.query.test.model.Farm;
 
@@ -60,6 +61,36 @@ public class EntityWhereOrderByTest extends GEN_BaseTestCase {
 
     @Test
     public void testEntityMultiAlias() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	final Farm farm4 = new Farm("Farm4");
+    	final Farm farm5 = new Farm("Farm5");
+
+    	final Farm other1 = new Farm("Other1");
+    	final Farm other2 = new Farm("Other2");
+    	final Farm other3 = new Farm("Other3");
+    	final Farm other4 = new Farm("Other4");
+    	final Farm other5 = new Farm("Other5");
+    	
+    	final Farm f = select.alias(Farm.class);
+    	
+    	final MultiBuilt<Farm> query = select.list(Farm.class)
+    			.where(f::getName).startsWith("Farm")
+    			.orderBy(f::getName)
+    			.build(); 
+
+    	// Store out of order
+    	store(other5, farm2, farm4, other2, other4, farm3, other1, farm5, farm1, other3)
+    	.checkListUnordered(
+    			query,
+    			
+    			() -> expected( 
+					new Farm(farm1.getId(), "Farm1"),
+					new Farm(farm2.getId(), "Farm2"),
+					new Farm(farm3.getId(), "Farm3"),
+					new Farm(farm4.getId(), "Farm4"),
+					new Farm(farm5.getId(), "Farm5")
+    			));
     }
 }
