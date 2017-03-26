230a29403cf0d704824a77bbf99d12caa7b29f8c
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-26 06:37:13 +0200

    Code cleanup

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/Short_Collector_Initial_Single_Aggregate_Any.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/Short_Collector_Initial_Single_Aggregate_Any.java
@@ -2,40 +2,7 @@ package com.neaterbits.query.sql.dsl.api;
 
 abstract class Short_Collector_Initial_Single_Aggregate_Any<MODEL, RESULT>
 	extends Short_Collector_Initial_Single_Any_Any_Base<MODEL, RESULT> {
-
-	/*
-		extends Short_Collector_WhereOrJoin_Base<
-
-			MODEL,
-			RESULT,
-			
-			
-			//IShortLogical_WhereOrJoin_SingleResult_Entity_Named<MODEL, RESULT, RESULT>,
-			//IShortLogical_WhereOrJoin_SingleResult_Entity_Alias<MODEL, RESULT>,
-						
-			
-			IShortResult_Mapped_Single_Named<MODEL, RESULT>,
-			IShortResult_Mapped_Single_Alias<MODEL, RESULT>,
-			
-			ISQLLogical_And_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLJoin_Condition_SingleResult_Named<MODEL, RESULT, Object, Object>,
-			ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>,
-			
-			ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
-			IShortJoin_Condition_SingleResult_Alias<MODEL, RESULT>,
-			ISQLLogical_AndOr_SingleResult_Alias<MODEL, RESULT>,
-			
-			Void
-		> {
-	*/
 	
-
 	Short_Collector_Initial_Single_Aggregate_Any(BaseQuery query, ModelCompiler<MODEL> modelCompiler, QueryResultAggregate result) {
 
 		super(query, modelCompiler);
@@ -45,81 +12,8 @@ abstract class Short_Collector_Initial_Single_Aggregate_Any<MODEL, RESULT>
 		}
 		
 		getQueryCollector().setResult(result);
-		
-		
-		
-		/*
-		super(
-				new QueryCollectorImpl<>(
-						query,
-						modelCompiler,
-						result),
-
-				new Collector_Clause(EConditionsClause.WHERE, ConditionsType.SINGLE));
-		*/
 	}
 
-	/*
-	@Override
-	final Collector_Or_Named<
-			MODEL,
-			RESULT,
-			
-			ISQLLogical_Or_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Named<MODEL, RESULT>,
-			ISharedProcessResult_After_GroupBy_Named<MODEL, RESULT>
-
-		> createNamedOrCollector() {
-
-		return new SQL_Collector_Or_NonProcessResult_Named<>(getThisInitial());
-	}
-
-	@Override
-	final Collector_And_Named<
-			MODEL,
-			RESULT,
-
-			ISQLLogical_And_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Named<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Named<MODEL, RESULT>,
-			ISharedProcessResult_After_GroupBy_Named<MODEL, RESULT>
-	
-		> createNamedAndCollector() {
-		
-		return new SQL_Collector_And_NonProcessResult_Named<>(getThisInitial());
-	}
-
-	@Override
-	final Collector_Or_Alias<
-			MODEL,
-			RESULT,
-			
-			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
-			ISharedProcessResult_After_GroupBy_Alias<MODEL, RESULT>
-	
-		> createAliasOrCollector() {
-
-		return new SQL_Collector_Or_NonProcessResult_Alias<>(getThisInitial());
-	}
-
-	@Override
-	final Collector_And_Alias<
-			MODEL,
-			RESULT,
-
-			ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>,
-			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
-			ISharedProcessResult_After_GroupBy_Alias<MODEL, RESULT>
-	
-		> createAliasAndCollector() {
-
-		return new SQL_Collector_And_NonProcessResult_Alias<>(getThisInitial());
-	}
-	*/
 
 	@Override
 	final Collector_GroupBy<MODEL, RESULT> createGroupByCollector(Collector_Base<MODEL> last, int[] groupByColumns,
