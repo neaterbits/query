f4209f47bd4ff4c25e96b5564df5e5f766966b57
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-03-31 19:21:32 +0200

    Remove unused code

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/Short_Collector_Single_Mapped_Alias_TypedJoin.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/Short_Collector_Single_Mapped_Alias_TypedJoin.java
@@ -12,97 +12,11 @@ final class Short_Collector_Single_Mapped_Alias_TypedJoin<MODEL, RESULT, JOIN_TY
 		super(select, result, queryCollector);
 	}
 	
-	
-
-/*	
-	
-	@Override
-	public <R extends Comparable<R>> IShortLogical_WhereOrJoin_SingleResult_Alias<MODEL, RESULT>
-			innerJoin(Supplier<R> from, Supplier<R> to) {
-				
-		return addInnerJoin(from, to);
-	}
-
-	@Override
-	public <JOIN_TO> IShortLogical_WhereOrJoin_SingleResult_Alias<MODEL, RESULT, JOIN_TYPE>
-			innerJoin(CollectionFunction<JOIN_TYPE, JOIN_TO> collection) {
-				
-		return addInnerJoin(collection);
-	}
-
-	@Override
-	public <JOIN_TO, R extends Comparable<R>> IShortLogical_WhereOrJoin_SingleResult_Alias<MODEL, RESULT> innerJoin(
-			Function<JOIN_TYPE, R> from, Function<JOIN_TO, R> to,
-			Consumer<IShortJoin_Sub_Alias<MODEL, RESULT, Void>> consumer) {
-		
-		return addInnerJoin(from, to, consumer);
-	}
-
-	@Override
-	public <JOIN_TO> IShortLogical_WhereOrJoin_SingleResult_Alias<MODEL, RESULT> innerJoin(
-			CollectionFunction<JOIN_TYPE, JOIN_TO> collection,
-			Consumer<IShortJoin_Sub_Named<MODEL, RESULT, JOIN_TO, Void>> consumer) {
-
-		return addInnerJoin(collection, consumer);
-	}
-
-	@Override
-	public <JOIN_TO, R extends Comparable<R>> IShortLogical_WhereOrJoin_SingleResult_Named<MODEL, RESULT, JOIN_TYPE>
-			leftJoin(Function<JOIN_TYPE, R> from, Function<JOIN_TO, R> to) {
-
-		return addLeftJoin(from, to);
-	}
-
-	@Override
-	public <JOIN_TO> IShortLogical_WhereOrJoin_SingleResult_Named<MODEL, RESULT, JOIN_TYPE>
-			leftJoin(CollectionFunction<JOIN_TYPE, JOIN_TO> collection) {
-
-		return addLeftJoin(collection);
-	}
-
-	@Override
-	public <JOIN_TO, R extends Comparable<R>> IShortLogical_WhereOrJoin_SingleResult_Named<MODEL, RESULT, JOIN_TYPE>
-			leftJoin(
-				Function<JOIN_TYPE, R> from, Function<JOIN_TO, R> to,
-				Consumer<IShortJoin_Sub_Named<MODEL, RESULT, JOIN_TO, Void>> consumer) {
-
-		return addLeftJoin(from, to, consumer);
-	}
-
-	@Override
-	public <JOIN_TO> IShortLogical_WhereOrJoin_SingleResult_Named<MODEL, RESULT, JOIN_TYPE> leftJoin(
-			CollectionFunction<JOIN_TYPE, JOIN_TO> collection,
-			Consumer<IShortJoin_Sub_Named<MODEL, RESULT, JOIN_TO, Void>> consumer) {
-
-		return addLeftJoin(collection, consumer);
-	}
-
-	@Override
-	public ISharedFunctions_Transform_Initial_Named<
-			MODEL,
-			RESULT, 
-			ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>,
-			ISharedCondition_Comparable_Common_All_Compilable<MODEL, RESULT, Short,ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>,
-			ISharedCondition_Comparable_Common_All_Compilable<MODEL, RESULT, Integer, ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>,
-			ISharedCondition_Comparable_Common_All_Compilable<MODEL, RESULT, Long, ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>,
-			ISharedCondition_Comparable_Common_All_Compilable<MODEL, RESULT, Double, ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>, 
-			ISharedCondition_Comparable_Common_All_Compilable<MODEL, RESULT, BigDecimal, ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>,
-			ISharedCondition_Comparable_String_All_Compilable<MODEL, RESULT, ISQLLogical_AndOr_SingleResult_Named<MODEL, RESULT>>
-		> where() {
-
-		throw new UnsupportedOperationException("TODO");
-	}
-	*/
-	
-	
-	
 
 	@Override
 	Collector_GroupBy<MODEL, RESULT> createGroupByCollector(Collector_Base<MODEL> last, int[] groupByColumns, Collector_Conditions_GroupBy<MODEL, RESULT, ?> collectorConditions) {
 		throw new UnsupportedOperationException("No group-by for single queries");
 	}
-
-
 
 	@Override
 	public ISharedFunctions_Transform_Initial_Alias<
