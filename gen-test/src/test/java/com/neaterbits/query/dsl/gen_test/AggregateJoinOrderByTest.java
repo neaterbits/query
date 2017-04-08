6c8ec293e0c94018b7c65fb2e65e0caf060f4004
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-08 06:36:39 +0200

    Test that aggregates cannot have orderBy() even after join

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateJoinOrderByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateJoinOrderByTest.java
@@ -3,6 +3,8 @@ package com.neaterbits.query.dsl.gen_test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.land.LandPlot;
 
 
 public class AggregateJoinOrderByTest extends GEN_BaseTestCase {
@@ -10,12 +12,33 @@ public class AggregateJoinOrderByTest extends GEN_BaseTestCase {
 
     @Test
     public void testAggregateSingleNamed() {
-        assertThat(true).isEqualTo(false);
+
+    	verifyIsCompilable(
+				"sum(LandPlot::getHectares)" +
+				".innerJoin(Farm::getLandPlots)");
+		
+		verifyIsNotCompilable(
+				"sum(LandPlot::getHectares)" +
+				".innerJoin(Farm::getLandPlots)" +
+				".orderBy(LandPlot::getHectares)");
     }
 
 
     @Test
     public void testAggregateSingleAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsCompilable(
+    			b -> b.add(Farm.class, "f")
+    				  .add(LandPlot.class, "l"),
+    				  
+				"sum(l::getHectares)" +
+				".innerJoin(f::getLandPlots, l)");
+		
+		verifyIsNotCompilable(
+    			b -> b.add(Farm.class, "f")
+				  .add(LandPlot.class, "l"),
+				  
+				"sum(l::getHectares)" +
+				".innerJoin(f::getLandPlots, l)" +
+				".orderBy(l::getHectares)");
     }
 }
