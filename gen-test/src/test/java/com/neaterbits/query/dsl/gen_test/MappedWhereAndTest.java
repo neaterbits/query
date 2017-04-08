6aba85cf87027edbd77159e97a1e99f081b078ff
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-08 05:52:34 +0200

    mapped where-and test

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereAndTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereAndTest.java
@@ -3,6 +3,9 @@ package com.neaterbits.query.dsl.gen_test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.MultiBuilt;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.mapped.FarmInfo;
 
 
 public class MappedWhereAndTest extends GEN_BaseTestCase {
@@ -10,24 +13,135 @@ public class MappedWhereAndTest extends GEN_BaseTestCase {
 
     @Test
     public void testMappedSingleNamed() {
-        assertThat(true).isEqualTo(false);
+
+    	final Farm farm1 = new Farm("Farm1", "main2", "sub3");
+    	final Farm farm2 = new Farm("Farm2", "main1", null);
+    	final Farm farm3 = new Farm("Farm3", "main2", "sub2");
+    	final Farm farm4 = new Farm("Farm4", "main3", "sub2");
+    	final Farm farm5 = new Farm("Farm5", "main2", "");
+
+    	final Farm other1 = new Farm("Other1", "other3", "sub1");
+    	final Farm other2 = new Farm("Other2", "other2", "sub2" );
+    	final Farm other3 = new Farm("Other3", "other3", "sub2");
+    	final Farm other4 = new Farm("Other4", "other1", null);
+    	final Farm other5 = new Farm("Other5", "other2", "sub1");
+
+    	MultiBuilt<FarmInfo> query = select.list(FarmInfo.class)
+    			.map(Farm::getName).to(FarmInfo::setName)
+    			.where(Farm::getName).startsWith("Farm")
+    			  .and(Farm::getFarmId).endsWith("1")
+    			.build(); 
+
+    	// Store out of order
+    	store(other5, farm2, farm4, other2, other4, farm3, other1, farm5, farm1, other3)
+    	.checkListUnordered(
+    			query,
+    			
+    			() -> expected( 
+					new FarmInfo("Farm2")
+    			));
     }
 
 
     @Test
     public void testMappedSingleAlias() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Farm1", "main2", "sub3");
+    	final Farm farm2 = new Farm("Farm2", "main1", null);
+    	final Farm farm3 = new Farm("Farm3", "main2", "sub2");
+    	final Farm farm4 = new Farm("Farm4", "main3", "sub2");
+    	final Farm farm5 = new Farm("Farm5", "main2", "");
+
+    	final Farm other1 = new Farm("Other1", "other3", "sub1");
+    	final Farm other2 = new Farm("Other2", "other2", "sub2" );
+    	final Farm other3 = new Farm("Other3", "other3", "sub2");
+    	final Farm other4 = new Farm("Other4", "other1", null);
+    	final Farm other5 = new Farm("Other5", "other2", "sub1");
+    	
+    	final Farm f = select.alias(Farm.class);
+
+    	MultiBuilt<FarmInfo> query = select.list(FarmInfo.class)
+    			.map(f::getName).to(FarmInfo::setName)
+    			.where(f::getName).startsWith("Farm")
+    			  .and(f::getFarmId).endsWith("1")
+    			.build(); 
+
+    	// Store out of order
+    	store(other5, farm2, farm4, other2, other4, farm3, other1, farm5, farm1, other3)
+    	.checkListUnordered(
+    			query,
+    			
+    			() -> expected( 
+					new FarmInfo("Farm2")
+    			));
     }
 
 
     @Test
     public void testMappedMultiNamed() {
-        assertThat(true).isEqualTo(false);
+
+    	final Farm farm1 = new Farm("Farm1", "main2", "sub3");
+    	final Farm farm2 = new Farm("Farm2", "main1", null);
+    	final Farm farm3 = new Farm("Farm3", "main2", "sub2");
+    	final Farm farm4 = new Farm("Farm4", "main3", "sub2");
+    	final Farm farm5 = new Farm("Farm5", "main2", "");
+
+    	final Farm other1 = new Farm("Other1", "other3", "sub1");
+    	final Farm other2 = new Farm("Other2", "other2", "sub2" );
+    	final Farm other3 = new Farm("Other3", "other3", "sub2");
+    	final Farm other4 = new Farm("Other4", "other1", null);
+    	final Farm other5 = new Farm("Other5", "otherFarm3", "sub2");
+
+    	MultiBuilt<FarmInfo> query = select.list(FarmInfo.class)
+    			.map(Farm::getName).to(FarmInfo::setName)
+    			.where(Farm::getFarmId).endsWith("3")
+    			  .and(Farm::getSubFarmId).endsWith("2")
+    			.build(); 
+
+    	// Store out of order
+    	store(other5, farm2, farm4, other2, other4, farm3, other1, farm5, farm1, other3)
+    	.checkListUnordered(
+    			query,
+    			
+    			() -> expected( 
+					new FarmInfo("Farm4"),
+					new FarmInfo("Other3"),
+					new FarmInfo("Other5")
+    			));
     }
 
 
     @Test
     public void testMappedMultiAlias() {
-        assertThat(true).isEqualTo(false);
+
+    	final Farm farm1 = new Farm("Farm1", "main2", "sub3");
+    	final Farm farm2 = new Farm("Farm2", "main1", null);
+    	final Farm farm3 = new Farm("Farm3", "main2", "sub2");
+    	final Farm farm4 = new Farm("Farm4", "main3", "sub2");
+    	final Farm farm5 = new Farm("Farm5", "main2", "");
+
+    	final Farm other1 = new Farm("Other1", "other3", "sub1");
+    	final Farm other2 = new Farm("Other2", "other2", "sub2" );
+    	final Farm other3 = new Farm("Other3", "other3", "sub2");
+    	final Farm other4 = new Farm("Other4", "other1", null);
+    	final Farm other5 = new Farm("Other5", "otherFarm3", "sub2");
+
+    	final Farm f = select.alias(Farm.class);
+
+    	MultiBuilt<FarmInfo> query = select.list(FarmInfo.class)
+    			.map(f::getName).to(FarmInfo::setName)
+    			.where(f::getFarmId).endsWith("3")
+    			  .and(f::getSubFarmId).endsWith("2")
+    			.build(); 
+
+    	// Store out of order
+    	store(other5, farm2, farm4, other2, other4, farm3, other1, farm5, farm1, other3)
+    	.checkListUnordered(
+    			query,
+    			
+    			() -> expected( 
+					new FarmInfo("Farm4"),
+					new FarmInfo("Other3"),
+					new FarmInfo("Other5")
+    			));
     }
 }
