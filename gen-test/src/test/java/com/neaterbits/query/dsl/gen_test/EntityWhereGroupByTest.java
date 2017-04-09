e499a83459f06f6eacd3f27ff8b02837e2b06d6c
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-09 07:25:19 +0200

    compile entity-where-groupby

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereGroupByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityWhereGroupByTest.java
@@ -1,8 +1,8 @@
 package com.neaterbits.query.dsl.gen_test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.test.model.Farm;
 
 
 public class EntityWhereGroupByTest extends GEN_BaseTestCase {
@@ -10,24 +10,57 @@ public class EntityWhereGroupByTest extends GEN_BaseTestCase {
 
     @Test
     public void testEntitySingleNamed() {
-        assertThat(true).isEqualTo(false);
+    	
+    	verifyIsCompilable(
+    		  "one(Farm.class)"
+    		+ ".where(Farm::getName).startsWith(\"Farm\")");
+    	
+    	verifyIsNotCompilable(
+      		   "one(Farm.class)"
+      		+ ".where(Farm::getName).startsWith(\"Farm\")"
+      	    + ".groupBy(Farm::getName)");
     }
 
 
     @Test
     public void testEntitySingleAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsCompilable(
+	    		Farm.class, "f",
+	      		  "one(f)"
+	      		+ ".where(f::getName).startsWith(\"Farm\")");
+      	
+      	verifyIsNotCompilable(
+	    		Farm.class, "f",
+        		   "one(f)"
+        		+ ".where(f::getName).startsWith(\"Farm\")"
+        	    + ".groupBy(f::getName)");
     }
 
 
     @Test
     public void testEntityMultiNamed() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsCompilable(
+      		  "list(Farm.class)"
+      		+ ".where(Farm::getName).startsWith(\"Farm\")");
+      	
+      	verifyIsNotCompilable(
+    		   "list(Farm.class)"
+    		+ ".where(Farm::getName).startsWith(\"Farm\")"
+    	    + ".groupBy(Farm::getName)");
     }
 
 
     @Test
     public void testEntityMultiAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsCompilable(
+	    		Farm.class, "f",
+	      		  "list(f)"
+	      		+ ".where(f::getName).startsWith(\"Farm\")");
+      	
+      	verifyIsNotCompilable(
+	    		Farm.class, "f",
+        		   "list(f)"
+        		+ ".where(f::getName).startsWith(\"Farm\")"
+        	    + ".groupBy(f::getName)");
     }
 }
