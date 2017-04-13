5389b7a583b8e941a1ab4d2fefc8195ac73cfec1
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-13 20:35:14 +0200

    Test and fix multiple and() function clauses for mapped

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereGroupByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedWhereGroupByTest.java
@@ -1,6 +1,5 @@
 package com.neaterbits.query.dsl.gen_test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
 import com.neaterbits.query.sql.dsl.api.MultiBuilt;
--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_And_MultiMapped_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_And_MultiMapped_Alias.java
@@ -1,7 +1,15 @@
 package com.neaterbits.query.sql.dsl.api;
 
 public interface ISQLLogical_And_MultiMapped_Alias<MODEL, RESULT>
-	extends ISharedLogical_And_Alias_Base<
+	extends
+	
+		ISharedLogical_And_Alias_Base<
+			MODEL,
+			RESULT,
+			ISQLLogical_And_MultiMapped_Alias<MODEL, RESULT>,
+			ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>>,
+			
+		ISharedLogical_And_Alias_Function<
 			MODEL,
 			RESULT,
 			ISQLLogical_And_MultiMapped_Alias<MODEL, RESULT>,
