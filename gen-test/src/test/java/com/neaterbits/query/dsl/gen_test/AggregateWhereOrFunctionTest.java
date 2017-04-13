921442c513971116e7d7f625d1b718bbcda70088
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-13 18:23:57 +0200

    Aggregate where-or() function test - added multi-or() test

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateWhereOrFunctionTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateWhereOrFunctionTest.java
@@ -1,6 +1,5 @@
 package com.neaterbits.query.dsl.gen_test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 
@@ -15,12 +14,11 @@ import com.neaterbits.query.test.model.land.Uncultivated;
 
 public class AggregateWhereOrFunctionTest extends GEN_BaseTestCase {
 
-
     @Test
     public void testAggregateSingleNamed() {
     	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
     	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
-    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("121.0"));
     	final LandPlot land4 = new Uncultivated(new BigDecimal("144.0"));
     	    	
     	// sum for all landplots
@@ -32,6 +30,17 @@ public class AggregateWhereOrFunctionTest extends GEN_BaseTestCase {
 
     	store(land1, land2, land3, land4)
     	.checkAggregate(query, new BigDecimal("193.80"));
+    	
+    	// Test that can have multiple or in a row
+    	query = select.sum(LandPlot::getHectares)
+    			
+    			.where(LandPlot::getHectares).isLessThan(new BigDecimal("39.9"))
+    			.or().sqrt(LandPlot::getHectares).isEqualTo(11.0)
+    			.or().sqrt(LandPlot::getHectares).isEqualTo(12.0)
+    			.build(); 
+
+        	store(land1, land2, land3, land4)
+        	.checkAggregate(query, new BigDecimal("274.30"));
     }
 
 
@@ -39,7 +48,7 @@ public class AggregateWhereOrFunctionTest extends GEN_BaseTestCase {
     public void testAggregateSingleAlias() {
     	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
     	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
-    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("121.0"));
     	final LandPlot land4 = new Uncultivated(new BigDecimal("144.0"));
 
     	final LandPlot l = select.alias(LandPlot.class);
@@ -49,9 +58,21 @@ public class AggregateWhereOrFunctionTest extends GEN_BaseTestCase {
     			
 			.where(l::getHectares).isLessThan(new BigDecimal("99.5"))
 			.or().sqrt(l::getHectares).isEqualTo(12.0)
+			
 			.build(); 
 
     	store(land1, land2, land3, land4)
     	.checkAggregate(query, new BigDecimal("193.80"));
+
+    	// Test that can have multiple or in a row
+    	query = select.sum(l::getHectares)
+    			
+    			.where(l::getHectares).isLessThan(new BigDecimal("39.9"))
+    			.or().sqrt(l::getHectares).isEqualTo(11.0)
+    			.or().sqrt(l::getHectares).isEqualTo(12.0)
+    			.build(); 
+
+        	store(land1, land2, land3, land4)
+        	.checkAggregate(query, new BigDecimal("274.30"));
     }
 }
--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_Or_NonProcessResult_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_Or_NonProcessResult_Alias.java
@@ -1,11 +1,20 @@
 package com.neaterbits.query.sql.dsl.api;
 
 public interface ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>
-	extends ISharedLogical_Or_Alias_Base<
+	extends
+	
+		ISharedLogical_Or_Alias_Base<
 					MODEL,
 					RESULT,
 					ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
 					ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>>,
-			ISharedCompileEndClause<MODEL> {
+					
+		ISharedLogical_Or_Alias_Function<
+					MODEL,
+					RESULT,
+					ISQLLogical_Or_NonProcessResult_Alias<MODEL, RESULT>,
+					ISQLLogical_And_NonProcessResult_Alias<MODEL, RESULT>>,
+
+		ISharedCompileEndClause<MODEL> {
 
 }
