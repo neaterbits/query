4447f63a4d8275376887c1c26ab4395d68aa3c8c
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-19 18:51:08 +0200

    entity-join missing tests

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityJoinTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityJoinTest.java
@@ -345,4 +345,76 @@ public class EntityJoinTest extends GEN_BaseTestCase {
     				new Forest		(land2.getId(), new BigDecimal("40.5")),
     				new Uncultivated(land3.getId(), new BigDecimal("100.5"))));
     }
+
+    @Test
+    public void testEntityMultiAlias_Farm_Inner() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm2");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+    	
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	final MultiBuilt<Farm> query
+    		= select.list(f)
+    			.innerJoin(f::getLandPlots, l)
+    			.build();
+    	
+    	store(farm1, farm2, farm3, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListUnordered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2")));
+    }
+
+    @Test
+    public void testEntityMultiAlias_Farm_Left() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	// only landplots that belong to farms, so land4 should not be included in sum
+    	// since doing innerjoin from farm to landplot
+    	final MultiBuilt<Farm> query
+    		= select.list(f)
+    			.leftJoin(f::getLandPlots, l)   
+    			.build();
+    	
+    	store(farm1, farm2, farm3, land4)
+    	.remove(land1, land2, land3)
+    	.checkListUnordered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2"),
+				new Farm(farm3.getId(), "Farm3")));
+    }
 }
