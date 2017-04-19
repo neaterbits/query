e6bbb12d1a57b1a72d24d7fa536b11432054a6ab
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-19 18:49:39 +0200

    entity-join-orderby

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityJoinOrderByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/EntityJoinOrderByTest.java
@@ -1,10 +1,18 @@
 package com.neaterbits.query.dsl.gen_test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.MultiBuilt;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.land.CropLand;
+import com.neaterbits.query.test.model.land.Forest;
+import com.neaterbits.query.test.model.land.LandPlot;
+import com.neaterbits.query.test.model.land.Uncultivated;
 
 
 public class EntityJoinOrderByTest extends GEN_BaseTestCase {
@@ -12,24 +20,261 @@ public class EntityJoinOrderByTest extends GEN_BaseTestCase {
 
     @Test
     public void testEntitySingleNamed() {
-        assertThat(true).isEqualTo(false);
-    }
+    	
+    	verifyIsNotCompilable(
+    			
+    			"one(LandPlot.class)" +
+    			".innerJoin(Farm::getLandPlots)" +
+    		    ".orderBy(LandPlot::getHectares)");
 
+    	verifyIsCompilable(
+    			"one(LandPlot.class)" +
+    			".innerJoin(Farm::getLandPlots)");
+    }
 
     @Test
     public void testEntitySingleAlias() {
-        assertThat(true).isEqualTo(false);
-    }
+    	
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"one(f)" +
+    			".innerJoin(f::getLandPlots, l)" +
+    			".orderBy(l::getHectares");
 
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"one(f)" +
+    			".innerJoin(f::getLandPlots, l)");
+    	
+    }
 
     @Test
     public void testEntityMultiNamed() {
-        assertThat(true).isEqualTo(false);
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+
+    	farm1.setLandPlots(Arrays.asList(land2, land1));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	
+    	final MultiBuilt<LandPlot> query
+    		= select.list(LandPlot.class)
+    			.innerJoin(Farm::getLandPlots)
+    			.orderBy(LandPlot::getHectares)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> 
+    			Arrays.asList(
+    				new CropLand	(land1.getId(), new BigDecimal("9.30")),
+    				new Forest		(land2.getId(), new BigDecimal("40.5")),
+    				new Uncultivated(land3.getId(), new BigDecimal("100.5"))));
     }
 
+    @Test
+    public void testEntityMultiNamed_Farm_Inner() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm2");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final MultiBuilt<Farm> query
+    		= select.list(Farm.class)
+    			.innerJoin(Farm::getLandPlots)
+    			.orderBy(Farm::getName)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2")));
+    }
+
+    @Test
+    public void testEntityMultiNamed_Farm_Left() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+    	
+    	// only landplots that belong to farms, so land4 should not be included in sum
+    	// since doing innerjoin from farm to landplot
+    	final MultiBuilt<Farm> query
+    		= select.list(Farm.class)
+    			.leftJoin(Farm::getLandPlots)
+    			.orderBy(Farm::getName)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2"),
+				new Farm(farm3.getId(), "Farm3")));
+    }
 
     @Test
     public void testEntityMultiAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(LandPlot.class)" +
+    			".innerJoin(f::getLandPlots, l)");
+
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(f)" +
+    			".innerJoin(f::getLandPlots, l)");
+
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+    	
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	final MultiBuilt<LandPlot> query
+    		= select.list(l)
+    			.innerJoin(f::getLandPlots, l)
+    			.orderBy(l::getHectares)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> 
+    			Arrays.asList(
+    				new CropLand	(land1.getId(), new BigDecimal("9.30")),
+    				new Forest		(land2.getId(), new BigDecimal("40.5")),
+    				new Uncultivated(land3.getId(), new BigDecimal("100.5"))));
     }
+
+    @Test
+    public void testEntityMultiAlias_Farm_Inner() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm2");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	final MultiBuilt<Farm> query
+    		= select.list(f)
+    			.innerJoin(f::getLandPlots, l)
+    			.orderBy(f::getName)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2")));
+    }
+    
+    @Test
+    public void testEntityMultiAlias_Farm_Left() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	// only landplots that belong to farms, so land4 should not be included in sum
+    	// since doing innerjoin from farm to landplot
+    	final MultiBuilt<Farm> query
+    		= select.list(f)
+    			.leftJoin(f::getLandPlots, l)
+    			.orderBy(f::getName)
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm1.getId(), "Farm1"),
+				new Farm(farm2.getId(), "Farm2"),
+				new Farm(farm3.getId(), "Farm3")));
+    }
+    
 }
