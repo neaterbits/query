2fbeffc7d5316947d3ca38f56066d60fbdf20f3f
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-22 13:39:34 +0200

    Added mapped-join-orderby test

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedJoinOrderByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/MappedJoinOrderByTest.java
@@ -1,35 +1,280 @@
 package com.neaterbits.query.dsl.gen_test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.math.BigDecimal;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.sql.dsl.api.MultiBuilt;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.land.CropLand;
+import com.neaterbits.query.test.model.land.Forest;
+import com.neaterbits.query.test.model.land.LandPlot;
+import com.neaterbits.query.test.model.land.Uncultivated;
+import com.neaterbits.query.test.model.mapped.FarmLand;
 
 
 public class MappedJoinOrderByTest extends GEN_BaseTestCase {
 
-
     @Test
     public void testMappedSingleNamed() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsNotCompilable(
+    			"one(FarmLand.class)" +
+    	    	".map(Farm::getName)	    .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares).to(FarmLand::setHectares)" +
+    			".innerJoin(Farm::getLandPlots)" +
+    	    	".orderBy(Farm::getName)");
+
+    	verifyIsNotCompilable(
+    			"one(FarmLand.class)" +
+    	    	".map(Farm::getName)	    .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares).to(FarmLand::setHectares)" +
+    			".leftJoin(Farm::getLandPlots)" +
+    	    	".orderBy(Farm::getName)");
+    	
+    	verifyIsCompilable(
+    			"list(FarmLand.class)" +
+    	    	".map(Farm::getName)	    .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares).to(FarmLand::setHectares)" +
+    			".innerJoin(Farm::getLandPlots)" +
+    	    	".orderBy(Farm::getName)");
+    	
+    	verifyIsCompilable(
+    			"one(FarmLand.class)" +
+    	    	".map(Farm::getName)	     .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares) .to(FarmLand::setHectares)" +
+    			".innerJoin(Farm::getLandPlots)");
+    	
     }
 
 
     @Test
     public void testMappedSingleAlias() {
-        assertThat(true).isEqualTo(false);
-    }
+    	
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"one(FarmLand.class)" +
+		    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+		    	".map(l::getHectares).to(FarmLand::setHectares)" +
+				".innerJoin(f::getLandPlots, l)" +
+		    	".orderBy(f::getName)");
+    	
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"one(FarmLand.class)" +
+		    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+		    	".map(l::getHectares).to(FarmLand::setHectares)" +
+				".leftJoin(f::getLandPlots, l)" +
+		    	".orderBy(f::getName)");
+    	
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(FarmLand.class)" +
+		    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+		    	".map(l::getHectares).to(FarmLand::setHectares)" +
+				".innerJoin(f::getLandPlots, l)" +
+		    	".orderBy(f::getName)");
 
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+
+    			"one(FarmLand.class)" +
+		    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+		    	".map(l::getHectares).to(FarmLand::setHectares)" +
+				".innerJoin(f::getLandPlots, l)");
+    }
 
     @Test
     public void testMappedMultiNamed() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(FarmLand.class)" +
+    	    	".map(Farm::getName)	 .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares) .to(FarmLand::setHectares)" +
+    			".innerJoin(Farm::getLandPlots)" +
+    	    	".orderBy(f::getName)");
+
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(FarmLand.class)" +
+    	    	".map(Farm::getName)	 .to(FarmLand::setFarmName)" +
+    	    	".map(LandPlot::getHectares) .to(FarmLand::setHectares)" +
+    			".innerJoin(Farm::getLandPlots)" +
+    	    	".orderBy(Farm::getName)");
+
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+    	
+    	final MultiBuilt<FarmLand> query
+    		= select.list(FarmLand.class)
+				.map(Farm::getName)			.to(FarmLand::setFarmName)
+				.map(LandPlot::getHectares) .to(FarmLand::setHectares)
+    			.innerJoin(Farm::getLandPlots)
+    			.orderBy(Farm::getName).desc().and(LandPlot::getHectares).asc()
+    			.build();
+    	
+    	store(farm2, farm3, farm1, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> 
+    			Arrays.asList(
+					new FarmLand("Farm2", new BigDecimal("100.5")),
+					new FarmLand("Farm1", new BigDecimal("9.30")),
+    				new FarmLand("Farm1", new BigDecimal("40.5"))));
     }
 
+    @Test
+    public void testMappedMultiNamed_Farm_Left() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+    	
+    	// only landplots that belong to farms, so land4 should not be included in sum
+    	// since doing innerjoin from farm to landplot
+    	final MultiBuilt<FarmLand> query
+    		= select.list(FarmLand.class)
+				.map(Farm::getName)			.to(FarmLand::setFarmName)
+				.map(LandPlot::getHectares) .to(FarmLand::setHectares)
+    			.leftJoin(Farm::getLandPlots)   
+    			.orderBy(Farm::getName).and(LandPlot::getHectares).desc()
+    			.build();
+    	
+    	store(farm1, farm2, farm3, land4)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new FarmLand("Farm1", new BigDecimal("40.5")),
+				new FarmLand("Farm1", new BigDecimal("9.30")),
+				new FarmLand("Farm2", new BigDecimal("100.5")),
+				new FarmLand("Farm3", null)));
+    }
+    
 
     @Test
     public void testMappedMultiAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsNotCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(FarmLand.class)" +
+    	    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+    	    	".map(l::getHectares) .to(FarmLand::setHectares)" +
+    			".innerJoin(f::getLandPlots), l" +
+    	    	".orderBy(Farm::getName)");
+
+    	verifyIsCompilable(
+    			a -> a.add(Farm.class, "f").add(LandPlot.class, "l"),
+    			
+    			"list(FarmLand.class)" +
+    	    	".map(f::getName)	 .to(FarmLand::setFarmName)" +
+    	    	".map(l::getHectares) .to(FarmLand::setHectares)" +
+    			".innerJoin(f::getLandPlots, l)" +
+    	    	".orderBy(f::getName)");
+
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	final MultiBuilt<FarmLand> query
+    		= select.list(FarmLand.class)
+				.map(f::getName)	.to(FarmLand::setFarmName)
+				.map(l::getHectares).to(FarmLand::setHectares)
+    			.innerJoin(f::getLandPlots, l)   
+    			.orderBy(f::getName).desc().and(l::getHectares).asc()
+    			.build();
+    	
+    	store(farm1, farm2, farm3, land4)
+    	// remove to avoid delete constraints when deleting Farm (not cascade)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> 
+    			Arrays.asList(
+					new FarmLand("Farm2", new BigDecimal("100.5")),
+					new FarmLand("Farm1", new BigDecimal("9.30")),
+					new FarmLand("Farm1", new BigDecimal("40.5"))
+					));
+    }
+
+    @Test
+    public void testMappedMultiAlias_Farm_Left() {
+    	final Farm farm1 = new Farm("Farm1");
+    	final Farm farm2 = new Farm("Farm2");
+    	final Farm farm3 = new Farm("Farm3");
+    	
+    	final LandPlot land1 = new CropLand(new BigDecimal("9.30"));
+    	final LandPlot land2 = new Forest(new BigDecimal("40.5"));
+    	final LandPlot land3 = new Uncultivated(new BigDecimal("100.5"));
+    	final LandPlot land4 = new Uncultivated(new BigDecimal("345.43"));
+
+    	farm1.setLandPlots(Arrays.asList(land1, land2));
+    	land1.setFarm(farm1);
+    	land2.setFarm(farm1);
+    	
+    	farm2.setLandPlots(Arrays.asList(land3));
+    	land3.setFarm(farm2);
+
+    	final Farm 	   f = select.alias(Farm.class);
+    	final LandPlot l = select.alias(LandPlot.class);
+    	
+    	// only landplots that belong to farms, so land4 should not be included in sum
+    	// since doing innerjoin from farm to landplot
+    	final MultiBuilt<FarmLand> query
+    		= select.list(FarmLand.class)
+				.map(f::getName)	.to(FarmLand::setFarmName)
+				.map(l::getHectares).to(FarmLand::setHectares)
+    			.leftJoin(f::getLandPlots, l)
+    			.orderBy(f::getName).and(l::getHectares).desc()
+    			.build();
+    	
+    	store(farm1, farm2, farm3, land4)
+    	.remove(land1, land2, land3)
+    	.checkListOrdered(query, () -> Arrays.asList(
+				new FarmLand("Farm1", new BigDecimal("40.5")),
+				new FarmLand("Farm1", new BigDecimal("9.30")),
+				new FarmLand("Farm2", new BigDecimal("100.5")),
+				new FarmLand("Farm3", null)));
     }
 }
