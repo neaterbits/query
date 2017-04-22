5ec7e6d59ae41b655a3ede73ad09fc657ad9d4e6
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-22 06:49:05 +0200

    Compilability test

--- gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateJoinWhereGroupByTest.java
+++ gen-test/src/test/java/com/neaterbits/query/dsl/gen_test/AggregateJoinWhereGroupByTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 import com.neaterbits.query.jpatest.GEN_BaseTestCase;
+import com.neaterbits.query.test.model.Farm;
+import com.neaterbits.query.test.model.land.LandPlot;
 
 
 public class AggregateJoinWhereGroupByTest extends GEN_BaseTestCase {
@@ -12,12 +14,33 @@ public class AggregateJoinWhereGroupByTest extends GEN_BaseTestCase {
 
     @Test
     public void testAggregateSingleNamed() {
-        assertThat(true).isEqualTo(false);
+
+    	verifyIsCompilable(
+				"sum(LandPlot::getHectares)" +
+				".innerJoin(Farm::getLandPlots)");
+		
+		verifyIsNotCompilable(
+				"sum(LandPlot::getHectares)" +
+				".innerJoin(Farm::getLandPlots)" +
+				".groupBy(LandPlot::getHectares)");
     }
 
 
     @Test
     public void testAggregateSingleAlias() {
-        assertThat(true).isEqualTo(false);
+    	verifyIsCompilable(
+    			b -> b.add(Farm.class, "f")
+    				  .add(LandPlot.class, "l"),
+    				  
+				"sum(l::getHectares)" +
+				".innerJoin(f::getLandPlots, l)");
+		
+		verifyIsNotCompilable(
+    			b -> b.add(Farm.class, "f")
+				  .add(LandPlot.class, "l"),
+				  
+				"sum(l::getHectares)" +
+				".innerJoin(f::getLandPlots, l)" +
+				".groupBy(l::getHectares)");
     }
 }
