a2f6831162739fddc0bf1ba6c0630dc2b531dfb4
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-29 13:36:46 +0200

    Removed ISharedOperands_Alias

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedResultOps_Numeric_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedResultOps_Numeric_Alias.java
@@ -10,11 +10,9 @@ public interface ISharedResultOps_Numeric_Alias<
 		extends
 			ISharedFunction_Next<MODEL, RESULT, RET>,
 			ISharedResultMapperTo<MODEL, RESULT, R, RET>, // Can always do "to" instead of op
-			ISharedOperands_Alias<
+			ISharedOperands_Numeric_Alias<
 					MODEL,
 					RESULT,
-					
-					R,
 					
 					RET,
 					
