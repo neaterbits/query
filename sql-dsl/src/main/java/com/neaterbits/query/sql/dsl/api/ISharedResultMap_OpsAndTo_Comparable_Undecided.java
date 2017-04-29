7f2f76fe76a8b2a3a72e19a6eab957164d6185b9
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-29 13:49:27 +0200

    Some changes for undecided

---
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedResultMap_OpsAndTo_Comparable_Undecided.java
@@ -0,0 +1,20 @@
+package com.neaterbits.query.sql.dsl.api;
+
+public interface ISharedResultMap_OpsAndTo_Comparable_Undecided<
+		MODEL,
+		RESULT,
+		R extends Comparable<R>,
+		RET extends ISharedFunction_After<MODEL, RESULT>>
+
+	extends ISharedResultMap_To_Comparable_Alias<MODEL, RESULT, R, RET>,
+			ISharedOperands_Numeric_Undecided<
+				MODEL,
+				RESULT,
+				
+				RET,
+				
+				ISharedResultMap_OpsAndTo_Comparable_Undecided<MODEL, RESULT, R, RET>
+			>
+	{
+
+}
