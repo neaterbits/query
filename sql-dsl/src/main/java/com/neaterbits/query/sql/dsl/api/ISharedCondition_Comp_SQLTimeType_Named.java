51ef4644f9a4a22a71a9c203f97b08c4bd4eaefe
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-30 14:05:31 +0200

    SQL time types are not Comparable

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_Comp_SQLTimeType_Named.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_Comp_SQLTimeType_Named.java
@@ -1,6 +1,6 @@
 package com.neaterbits.query.sql.dsl.api;
 
-public interface ISharedCondition_Comp_SQLTimeType_Named<MODEL, RESULT, R extends Comparable<R>, L extends ISharedLogical_Base<MODEL, RESULT>> 
+public interface ISharedCondition_Comp_SQLTimeType_Named<MODEL, RESULT, R, L extends ISharedLogical_Base<MODEL, RESULT>> 
 
 		extends ISharedComparison_SQLTimeType_All<MODEL, RESULT, R, L> {
 
--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_SQLTimeType_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_SQLTimeType_Alias.java
@@ -1,6 +1,6 @@
 package com.neaterbits.query.sql.dsl.api;
 
-public interface ISharedCondition_OpsAndComp_SQLTimeType_Alias<MODEL, RESULT, R extends Comparable<R>, L extends ISharedLogical_Base<MODEL, RESULT>>
+public interface ISharedCondition_OpsAndComp_SQLTimeType_Alias<MODEL, RESULT, R, L extends ISharedLogical_Base<MODEL, RESULT>>
 	extends ISharedCondition_Comp_SQLTimeType_Alias<MODEL, RESULT, R, ISharedLogical_Base<MODEL,RESULT>> {
 
 }
