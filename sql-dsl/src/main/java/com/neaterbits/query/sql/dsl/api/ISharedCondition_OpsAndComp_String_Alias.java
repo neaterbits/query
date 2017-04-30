edb588f7c9b9bba7caae5de0ce1a208601e2b6e5
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-30 14:11:01 +0200

    Added operand interfaces for conditions

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_String_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_String_Alias.java
@@ -2,6 +2,14 @@ package com.neaterbits.query.sql.dsl.api;
 
 public interface ISharedCondition_OpsAndComp_String_Alias<MODEL, RESULT, L extends ISharedLogical_Base<MODEL, RESULT>>
 	
-	extends ISharedCondition_Comp_String_Alias<MODEL, RESULT, L> {
+	extends ISharedCondition_Comp_String_Alias<MODEL, RESULT, L>,
+			ISharedOperands_String_Alias_All<
+				MODEL,
+				RESULT,
+				
+				L,
+				
+				ISharedCondition_OpsAndComp_String_Alias<MODEL, RESULT, L>
+			> {
 
 }
--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_String_Named.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedCondition_OpsAndComp_String_Named.java
@@ -2,6 +2,14 @@ package com.neaterbits.query.sql.dsl.api;
 
 public interface ISharedCondition_OpsAndComp_String_Named<MODEL, RESULT, L extends ISharedLogical_Base<MODEL, RESULT>>
 
-		extends ISharedCondition_Comp_String_Named<MODEL, RESULT, L> {
-	
+	extends ISharedCondition_Comp_String_Named<MODEL, RESULT, L>,
+			ISharedOperands_String_Named_All<
+				MODEL,
+				RESULT,
+				
+				L,
+				
+				ISharedCondition_OpsAndComp_String_Named<MODEL, RESULT, L>
+			> {
+
 }
