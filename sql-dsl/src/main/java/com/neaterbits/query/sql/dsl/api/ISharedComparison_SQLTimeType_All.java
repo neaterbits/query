7d7420710ce6c8861c22986a44de083d30e5f13f
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-04-30 14:00:58 +0200

    Base comparison interfaces

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedComparison_SQLTimeType_All.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISharedComparison_SQLTimeType_All.java
@@ -1,5 +1,6 @@
 package com.neaterbits.query.sql.dsl.api;
 
+// TODO: split in named and alias?
 public interface ISharedComparison_SQLTimeType_All<MODEL, RESULT, T, L extends ISharedLogical_Base<MODEL, RESULT>>
 		extends ISharedComparison_SQLTimeType_Value<MODEL, RESULT, T, L>,
 				ISharedComparison_SQLTimeType_Param<MODEL, RESULT, L>{
