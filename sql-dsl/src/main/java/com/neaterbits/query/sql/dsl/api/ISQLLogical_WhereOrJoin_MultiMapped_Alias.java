c3e1018e4be2bfb80e84760f568604e9c5df32e0
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2017-06-09 20:32:49 +0200

    No and() after map() for alias queries

--- sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_WhereOrJoin_MultiMapped_Alias.java
+++ sql-dsl/src/main/java/com/neaterbits/query/sql/dsl/api/ISQLLogical_WhereOrJoin_MultiMapped_Alias.java
@@ -10,7 +10,6 @@ public interface ISQLLogical_WhereOrJoin_MultiMapped_Alias<
 		ISQLLogical_Where_MultiMapped_Alias<MODEL, RESULT>,
 		ISQLJoin_MultiMapped_Alias<MODEL, RESULT>,
 		ISQLJoin_Condition_MultiMapped_Alias<MODEL, RESULT, JOIN_CONDITION>,
-		ISQLLogical_AndOr_MultiMapped_Alias<MODEL, RESULT>,
 		ISQLProcessResult_Alias<MODEL, RESULT> {
 
 }
